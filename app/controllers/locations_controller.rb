f4cc967b79f2b0b0572928d82ba0e3f8b67bf286
MartinCrane <martin.crane@gmail.com>
2017-03-13 15:36:44 -0400

    address confirm

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -7,6 +7,7 @@ class LocationsController < ApplicationController
   end
 
   def new
+
     @location = Location.new
   end
 
@@ -14,52 +15,49 @@ class LocationsController < ApplicationController
     location = Location.new(location_params)
     location.gather_api_location_data
     @location = location
-    #save user input to compare against confirm page's address
     session[:temp_location] = @location
     render "confirm"
   end
 
   def create
-    #new object based off of the confirm form
+    flash[:error] = ''
     confirm_location = Location.new(location_params)
-    #compare confirm-form response to user's original input
+    #is edited info differetn than first search?
     if compare_location_info(session[:temp_location], confirm_location)
-      confirm_location.save
-      session[:temp_location] = {}
-      redirect_to confirm_location
+
+      if confirm_location.save
+        session[:temp_location] = {}
+        redirect_to confirm_location
+      else
+        flash[:error] = "Location already exists, bro"
+        @location = confirm_location
+        render "confirm"
+        return
+      end
     else
-      #hit API to update edited address information
       confirm_location.reformat_address
       confirm_location.gather_api_location_data
       @location=confirm_location
       session[:temp_location] = @location
-      flash[:error] = "updated location"
+      flash[:error] = "Thank you. Does this look correct?"
       render "confirm"
       return
     end
+    # flash[:error] = "Location entry invalid. Please supply name and address."
   end
 
   def show
   end
 
   def edit
+
   end
 
   def update
-    location = Location.find_by(id: params[:id])
-    #create new ruby object based on edit-form response
-    new_location = Location.new(location_params)
-    new_location.reformat_address
-    new_location.gather_api_location_data
-    #create a hash for edit form response that includes google API data
-    new_location_params = update_hash(new_location)
-      if location.update(new_location_params) #validates address uniqueness
-        flash[:message] = "Location has been updated."
-        redirect_to location
-      else
-        flash[:error] = "Address " + location.errors.messages[:formatted_address][0]
-        redirect_to edit_location_path
-      end
+    location = set_location
+    location.update(location_params)
+    flash[:message] = "Location has been updated."
+    redirect_to location
   end
 
   def destroy
@@ -81,18 +79,5 @@ class LocationsController < ApplicationController
     argument1["road"] == argument2.road &&
     argument1["state"] == argument2.state &&
     argument1["country"] == argument2.country
-  end
-
-  def update_hash(location_object)
-    hash = {}
-    hash[:street_number] = location_object.street_number
-    hash[:road] = location_object.road
-    hash[:formatted_address] = location_object.formatted_address
-    hash[:latitude] = location_object.latitude
-    hash[:longitude] = location_object.longitude
-    hash[:state] = location_object.state
-    hash[:country] = location_object.country
-    hash[:name] = location_object.name
-    hash
   end
 end
