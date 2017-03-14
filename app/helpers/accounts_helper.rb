9a3084036a60d25a616ab012a61584ad919f7ce9
mwei2509 <m2dl89@gmail.com>
2017-03-14 12:05:56 -0400

    small changes

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,15 +1,13 @@
 module AccountsHelper
   def present_event(account)
     date = Date.today
-    {
-      "0" => Event.find_by_date(date, account),
-      "1" => Event.find_by_date(date + 1, account),
-      "2" => Event.find_by_date(date + 2, account),
-      "3" => Event.find_by_date(date +3, account),
-      "4" => Event.find_by_date(date + 4, account),
-      "5" => Event.find_by_date(date + 5, account),
-      "6" => Event.find_by_date(date + 6, account),
-      "7" => Event.find_by_date(date + 7, account)
-    }
+    hash={}
+    i=0
+    while i<8
+      hash["#{i.to_s}"] = Event.find_by_date(date+i, account)
+      i+=1
+    end
+    hash
   end
+
 end
