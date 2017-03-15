8797337f35f10b14b6743bc6066fad14559a9509
MartinCrane <martin.crane@gmail.com>
2017-03-15 09:30:01 -0400

    location edit

--- app/models/location.rb
+++ app/models/location.rb
@@ -18,9 +18,20 @@ class Location < ApplicationRecord
     compare_location_info(temp_location, self)
   end
 
-  def update_api_data
-    self.reformat_address
-    self.gather_api_location_data
-    self.save
+  def update_location(params)
+    new_location = Location.new
+    new_location.update(params)
+    new_location.reformat_address
+    new_location.gather_api_location_data
+    self.name = new_location.name
+    self.address = new_location.address
+    self.street_number = new_location.street_number
+    self.road = new_location.road
+    self.city = new_location.city
+    self.state = new_location.state
+    self.country = new_location.country
+    self.latitude = new_location.latitude
+    self.longitude = new_location.longitude
+    self.formatted_address = new_location.formatted_address
   end
 end
