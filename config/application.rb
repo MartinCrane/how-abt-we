b807bf9fc8b9fd8f195e135e5b14e4322ed9567f
MartinCrane <martin.crane@gmail.com>
2017-03-15 22:58:45 -0400

    static_assets

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
-config.serve_static_assets = true
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module HowAbtWe
   class Application < Rails::Application
     config.autoload_paths += %W(#{config.root}/lib)
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
